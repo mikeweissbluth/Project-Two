edb69109ab3de7472a2b39fb42c8c756d48ab99d
Alejandra Rubalcava <arubalcava@group.on>
2018-06-02 11:22:10 -0500

    chems are in array successfully, need to route result to server

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,17 +6,35 @@ var _ = require('lodash');
 module.exports = function (app) {
     app.get("/api/all", function (request, response) {
         var dbQuery = "SELECT f.FACILITY_NAME, f.FACILITY_ID, f.LATITUDE, f.LONGITUDE, c.CHEM_NAME FROM environment_db.facility_info f LEFT JOIN environment_db.chemical_info c ON f.FACILITY_ID = c.FACILITY_ID";
-    connection.query(dbQuery, function (err, result) {
-      if (err) throw err;
-      //console.log(result);
-      response.json(result);
-      _.uniq(json.result, function(item, key, a) { 
-        return item.a;
-        console.log(item.a);
+        connection.query(dbQuery, function (err, result) {
+            if (err) throw err;
+            //console.log(result);
+            //response.json(result);
+
+            var output = [];
+
+            result.forEach(function (value) {
+                var existing = output.filter(function (v, i) {
+                    return v.FACILITY_ID == value.FACILITY_ID;
+                });
+                if (existing.length) {
+                    var existingIndex = output.indexOf(existing[0]);
+                    output[existingIndex].CHEM_NAME = output[existingIndex].CHEM_NAME.concat(value.CHEM_NAME);
+                } else {
+                    if (typeof value.CHEM_NAME == 'string')
+                    value.CHEM_NAME = [value.CHEM_NAME];
+                    output.push(value);
+                }
+            });
+
+            console.dir(output);
+           
+            
+        });
+
+        
     });
 
-    });
 
-    });
 };
 
