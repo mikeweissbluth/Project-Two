f41b57c5b0f73b5dd5619e0170b9cbdb05eeb574
Karen E Gutzman <keg827@fsm.northwestern.edu>
2018-06-04 18:56:49 -0500

    added carcinogen

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,7 +5,7 @@ var _ = require('lodash');
 
 module.exports = function (app) {
     app.get("/api/all", function (request, response) {
-        var dbQuery = "SELECT f.FACILITY_NAME, f.FACILITY_ID, f.LATITUDE, f.LONGITUDE, c.CHEM_NAME FROM environment_db.facility_info f LEFT JOIN environment_db.chemical_info c ON f.FACILITY_ID = c.FACILITY_ID";
+        var dbQuery = "SELECT f.FACILITY_NAME, f.FACILITY_ID, f.LATITUDE, f.LONGITUDE, c.CHEM_NAME, c.CARCINOGEN, c.CLEAN_AIR FROM environment_db.facility_info f LEFT JOIN environment_db.chemical_info c ON f.FACILITY_ID = c.FACILITY_ID";
         connection.query(dbQuery, function (err, result) {
             if (err) throw err;
             //console.log(result);
