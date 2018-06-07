546201a0e27c71349adea41fb12eb3e9bdf0456c
Alejandra Rubalcava <arubalcava@group.on>
2018-06-06 21:37:44 -0500

    connection

--- config/connection.js
+++ config/connection.js
@@ -7,10 +7,10 @@ var mysql = require("mysql");
 
 // Set up our connection information
 var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "root",
+  port: 5432,
+  host: "ec2-107-21-255-2.compute-1.amazonaws.com",
+  user: "hqzhtxqhwspnks",
+  password: "ebaaf4a553a13efe03ccd0c77451ffa22fdb831fd78562205f6344a4e858d3c8",
   database: "environment_db",
   multipleStatements: true
 });
