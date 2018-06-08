9c5acea93ea1cdbbcb02507bbcad32afd3ecacb8
Alejandra Rubalcava <arubalcava@group.on>
2018-06-08 09:49:43 -0500

    added jaws code in connection.js

--- config/connection.js
+++ config/connection.js
@@ -6,14 +6,17 @@
 var mysql = require("mysql");
 
 // Set up our connection information
-var connection = mysql.createConnection({
-  port: 5432,
-  host: "ec2-107-21-255-2.compute-1.amazonaws.com",
-  user: "hqzhtxqhwspnks",
-  password: "ebaaf4a553a13efe03ccd0c77451ffa22fdb831fd78562205f6344a4e858d3c8",
-  database: "environment_db",
-  multipleStatements: true
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "environment_db"
+  });
+};
 
 
 // Connect to the database
